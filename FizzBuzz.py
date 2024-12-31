d79bf6dc39e73291cd5d3bff9f0e0663e8ed1949
JacksonAHannan <jacksonahannan@outlook.com>
2024-12-30 20:06:16 -0600

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,10 @@
+for i in range (1,101):
+    print(i)
+    if i % 3 == 0 and i % 5 != 0:
+        print("Fizz")
+    
+    elif i % 3 != 0 and i % 5 == 0:
+        print("Buzz")
+    
+    elif i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
